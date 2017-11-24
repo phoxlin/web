00253ec3245a988efd3dbb739480e825d28339af
陈林 <colin209@163.com>
2017-11-24 09:02:26 +0800

    core

---
+++ webserver/src/com/core/Assignee.java
@@ -0,0 +1,105 @@
+package com.core;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/11/24.
+ */
+public class Assignee {
+    private List<String> ids = new ArrayList();
+    private List<String> roles = new ArrayList();
+
+
+    public List<String> getIds() {
+        return this.ids;
+    }
+
+    public List<String> getRoles() {
+        return this.roles;
+    }
+
+    public void addId(String id) {
+        this.ids.add(id);
+    }
+
+    public void addRole(String role) {
+        this.roles.add(role);
+    }
+
+    public JSONArray toJson(Connection conn) throws Exception {
+        JSONArray li = new JSONArray();
+        EntityImpl en = new EntityImpl(conn);
+        int size;
+        int i;
+        String id;
+        String user_name;
+        JSONObject o;
+        if(this.ids.size() > 0) {
+            size = en.executeQuery("select a.id,a.user_name from sys_user a where a.id in (" + Utils.getListString("?", this.ids.size()) + ")", this.ids.toArray());
+
+            for(i = 0; i < size; ++i) {
+                id = en.getStringValue("id", i);
+                user_name = en.getStringValue("user_name", i);
+                o = new JSONObject();
+                o.put("code", id);
+                o.put("note", user_name);
+                li.put(o);
+            }
+        }
+
+        if(this.roles.size() > 0) {
+            size = en.executeQuery("select a.id,a.user_name from sys_user a ,sys_user_role b where b.fk_user_id=a.id and b.fk_role_code in(" + Utils.getListString("?", this.roles.size()) + ")", this.roles.toArray());
+
+            for(i = 0; i < size; ++i) {
+                id = en.getStringValue("id", i);
+                user_name = en.getStringValue("user_name", i);
+                o = new JSONObject();
+                o.put("code", id);
+                o.put("note", user_name);
+                li.put(o);
+            }
+        }
+
+        if(li.length() <= 0) {
+            size = en.executeQuery("select a.id,a.user_name from sys_user a");
+
+            for(i = 0; i < size; ++i) {
+                id = en.getStringValue("id", i);
+                user_name = en.getStringValue("user_name", i);
+                o = new JSONObject();
+                o.put("code", id);
+                o.put("note", user_name);
+                li.put(o);
+            }
+        }
+
+        return li;
+    }
+
+    public JSONObject toJson() {
+        JSONObject o = new JSONObject();
+        JSONArray ids = new JSONArray();
+        JSONArray roles = new JSONArray();
+        Iterator var5 = this.ids.iterator();
+
+        String role;
+        while(var5.hasNext()) {
+            role = (String)var5.next();
+            ids.put(role);
+        }
+
+        var5 = this.roles.iterator();
+
+        while(var5.hasNext()) {
+            role = (String)var5.next();
+            roles.put(role);
+        }
+
+        o.put("ids", ids);
+        o.put("roles", roles);
+        return o;
+    }
+}
---
+++ webserver/src/com/core/JhOrg.java
@@ -0,0 +1,156 @@
+package com.core;
+
+/**
+ * Created by Administrator on 2017/11/24.
+ */
+public class JhOrg {
+    private String id;
+    private String pid;
+    private boolean top;
+    private String name;
+    private String adminName;
+    private String adminPwd;
+    private String area;
+    private String area_name;
+    private String addr;
+    private String bossName;
+    private String bossPhone;
+    private String linkman;
+    private String linkPhone;
+    private String linkman2;
+    private String linkPhone2;
+    private String appId;
+
+    public JhOrg() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPid() {
+        return this.pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public boolean isTop() {
+        if(this.pid != null && this.pid.length() > 0 && !this.pid.equals("-1")) {
+            this.top = false;
+        } else {
+            this.top = true;
+        }
+
+        return this.top;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdminName() {
+        return this.adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getAdminPwd() {
+        return this.adminPwd;
+    }
+
+    public void setAdminPwd(String adminPwd) {
+        this.adminPwd = adminPwd;
+    }
+
+    public String getArea() {
+        return this.area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getArea_name() {
+        return this.area_name;
+    }
+
+    public void setArea_name(String area_name) {
+        this.area_name = area_name;
+    }
+
+    public String getAddr() {
+        return this.addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getBossName() {
+        return this.bossName;
+    }
+
+    public void setBossName(String bossName) {
+        this.bossName = bossName;
+    }
+
+    public String getBossPhone() {
+        return this.bossPhone;
+    }
+
+    public void setBossPhone(String bossPhone) {
+        this.bossPhone = bossPhone;
+    }
+
+    public String getLinkman() {
+        return this.linkman;
+    }
+
+    public void setLinkman(String linkman) {
+        this.linkman = linkman;
+    }
+
+    public String getLinkPhone() {
+        return this.linkPhone;
+    }
+
+    public void setLinkPhone(String linkPhone) {
+        this.linkPhone = linkPhone;
+    }
+
+    public String getLinkman2() {
+        return this.linkman2;
+    }
+
+    public void setLinkman2(String linkman2) {
+        this.linkman2 = linkman2;
+    }
+
+    public String getLinkPhone2() {
+        return this.linkPhone2;
+    }
+
+    public void setLinkPhone2(String linkPhone2) {
+        this.linkPhone2 = linkPhone2;
+    }
+
+    public String getAppId() {
+        return this.appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+}
